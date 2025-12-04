818402439370468cd65fa7580bc3a24cdf63bf81
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-04 21:22:51 +0530

    Create method save in NewUser

--- src/main/java/controller/NewUserFormController.java
+++ src/main/java/controller/NewUserFormController.java
@@ -5,12 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Student;
 
 import java.io.IOException;
 
@@ -48,8 +50,34 @@ public class NewUserFormController {
     @FXML
     void createAccountOnAction(ActionEvent event) {
 
+        String id=getNextId(getLastId());
+        String email=txtEmail.getText();
+        String password=txtPassword.getText();
+
+        newUser.save(new Student(id,email,password));
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Successfully Created");
+        alert.setHeaderText(null);
+        alert.setContentText("Your Student Id is "+id);
+        alert.showAndWait();
 
 
+    }
+
+    private String getNextId(String lastId) {
+        if (lastId == null || lastId.isEmpty()) {
+            return "S001";
+        }
+
+        int number = Integer.parseInt(lastId.substring(1));
+        number++;
+
+        return String.format("S%03d", number);
+    }
+
+    private String getLastId() {
+        return newUser.getLastId();
     }
 
     @FXML
