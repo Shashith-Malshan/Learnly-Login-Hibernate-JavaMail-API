f5eca87420d060e5669f7bab1ee1b5535e3551f8
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-04 21:25:22 +0530

    Create method getStudentById in Login

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,21 +1,25 @@
 package controller;
 
+import controller.impl.LoginImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Student;
 
 import java.io.IOException;
 
 public class LoginFormController {
 
     Stage stage=new Stage();
+    Login login=new LoginImpl();
 
     @FXML
     private Button btnLogin;
@@ -31,6 +35,21 @@ public class LoginFormController {
 
     @FXML
     void loginOnAction(ActionEvent event) {
+        String id=txtUsername.getText();
+        String password=txtPassword.getText();
+
+        Student student=login.getStudentById(id);
+        if(student.getPassword().equals(password)){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Login Status");
+            alert.setHeaderText(null);
+            alert.setContentText("Successfully logged in!");
+            alert.showAndWait();
+        }
+
+
+
+
 
 
     }
