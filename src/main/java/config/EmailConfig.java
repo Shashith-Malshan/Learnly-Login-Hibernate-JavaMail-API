7f71feed976f54b2f0a54853cffe2964b36916fb
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-05 07:55:46 +0530

    Add EmailConfig

---
+++ src/main/java/config/EmailConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailConfig {
+
+    public void sendOtpEmail(String toEmail, String otp) throws Exception {
+
+        String from = "";
+        String appPassword = "";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, appPassword);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+        message.setSubject("Your Learnly OTP Verification");
+        message.setText("Your OTP is: " + otp + "\n\nThis code is valid for 5 minutes.");
+
+        Transport.send(message);
+    }
+
+
+}
